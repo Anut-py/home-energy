6e468e32fbc9e0ad3cbee07527777bd6897ee11a
Anut-py <72886192+Anut-py@users.noreply.github.com>
2021-12-02 09:14:42 -0500

    Update start.js

--- start.js
+++ start.js
@@ -3,6 +3,7 @@ const path = require("path");
 const app = express();
 
 app.use(express.static(path.join(__dirname, "dist", "home-energy")));
+app.use((req, res) => res.sendFile(path.join(__dirname, "dist", "home-energy", "index.html")));
 
 const port = process.env.PORT || 4200;
 
