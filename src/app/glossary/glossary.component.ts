5b76eb479236333ad2b9d33d760eee4f7d899433
Anut-py <72886192+Anut-py@users.noreply.github.com>
2021-12-08 12:38:11 -0500

    Added glossary content

--- src/app/glossary/glossary.component.ts
+++ src/app/glossary/glossary.component.ts
@@ -6,6 +6,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./glossary.component.scss']
 })
 export class GlossaryComponent implements OnInit {
+  terms = [
+    {
+      word: 'energy efficient',
+      type: 'adjective',
+      definition: 'Describing something as having a high energy efficiency'
+    },
+    {
+      word: 'energy efficieny',
+      type: 'noun',
+      definition: 'A percentage that shows how much input energy is converted to useful output'
+    },
+    {
+      word: 'insulator',
+      type: 'noun',
+      definition: 'Something that doesn\'t let heat through easily'
+    },
+    {
+      word: 'solar energy',
+      type: 'noun',
+      definition: 'Energy that is produced by the sun (light and heat)'
+    },
+    {
+      word: 'energy transfer',
+      type: 'noun',
+      definition: 'The process of moving energy from one place to another'
+    },
+    {
+      word: 'energy transformation',
+      type: 'noun',
+      definition: 'The process of converting energy from one form to another'
+    },
+    {
+      word: 'side product',
+      type: 'noun',
+      definition: 'An output product of a process that is not the intended output'
+    }
+  ]
 
   constructor() { }
 
